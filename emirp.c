e2b625ad2f00cf2ef910703cbd73b83baec9bea2
David Schneider <david.schneider@postmail.ch>
2015-03-07 16:00:13 +0100

    test revers two digits number

--- emirp.c
+++ emirp.c
@@ -2,5 +2,12 @@
 
 int revers_digits(int number)
 {
-  return number;
+  int reversed = number % 10;
+  number /= 10;
+  if (number > 0)
+  {
+    reversed *= 10;
+    reversed += number;
+  }
+  return reversed;
 }
--- emirp_test.c
+++ emirp_test.c
@@ -4,11 +4,19 @@
 #include "emirp.h"
 
 static const int one_digit_number = 5;
+static const int two_digit_number = 15;
+static const int two_digit_number_reversed = 51;
 
 static void revers_digits_one_digit()
 {
   int reversed = revers_digits(one_digit_number);
   CU_ASSERT_EQUAL(reversed, one_digit_number);
+}
+
+static void revers_digits_two_digit()
+{
+  int reversed = revers_digits(two_digit_number);
+  CU_ASSERT_EQUAL(reversed, two_digit_number_reversed);
 }
 
 int main()
@@ -17,8 +25,10 @@ int main()
   CU_basic_set_mode(CU_BRM_VERBOSE);
 
   CU_pSuite suite = CU_add_suite("revers digits", NULL, NULL);
-  CU_add_test(suite, "single digit numbers should return the same number",
+  CU_add_test(suite, "single-digit numbers should return the same number",
               revers_digits_one_digit);
+  CU_add_test(suite, "two-digit numbers should return reversed number",
+              revers_digits_two_digit);
 
   CU_ErrorCode error_code = CU_basic_run_tests();
   CU_cleanup_registry();
