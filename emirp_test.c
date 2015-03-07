923e3dbafa658f2b2180c34102f62c9b80aac63a
David Schneider <david.schneider@postmail.ch>
2015-03-07 15:44:42 +0100

    add example test

--- emirp_test.c
+++ emirp_test.c
@@ -1,16 +1,21 @@
 #include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 
 #include "emirp.h"
 
+void test1()
+{
+  CU_ASSERT(0);
+}
+
 int main()
 {
-  if (CU_initialize_registry()) 
-  {
-    printf("Initialization of Test Registry failed.\n");
-  }
-  else
-  {
-    CU_basic_run_tests();
-    CU_cleanup_registry();
-  }
+  CU_initialize_registry();
+  CU_pSuite suite = CU_add_suite("suite", NULL, NULL);
+  CU_add_test(suite, "test1", test1);
+
+  CU_ErrorCode error_code = CU_basic_run_tests();
+  CU_cleanup_registry();
+
+  return error_code;
 }
