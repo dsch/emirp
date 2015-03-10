562d3bef638aa64a005dbd10001f9bd05a4b5c22
David Schneider <d.schneider@dimoco.at>
2015-03-10 14:01:14 +0100

    refactoring

--- emirp.c
+++ emirp.c
@@ -2,16 +2,23 @@
 
 static bool has_no_odd_factor(N number)
 {
-  return ((number % 3) != 0) &&
-          ((number % 5) != 0);
+  N odd_factor = 3U;
+  do
+  {
+    if ((number % odd_factor) == 0U)
+    {
+      return false;
+    }
+    odd_factor += 2U;
+  } while ((odd_factor * odd_factor) <= number);
+  return true;
 }
 
-
-
-bool is_emirp(N number) {
+bool is_emirp(N number)
+{
   static const N base = 10U;
   return (number >= base) &&
-          ((number % 2) != 0) &&
+          ((number % 2U) != 0U) &&
           has_no_odd_factor(number) &&
           ((number % base) != (number / base));
 }
