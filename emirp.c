7194e0876f29929dc6b8bc5d5dffb0feb18a45b9
David Schneider <d.schneider@dimoco.at>
2015-03-10 13:57:19 +0100

    refactoring; extract has_no_odd_factor

--- emirp.c
+++ emirp.c
@@ -1,10 +1,17 @@
 #include "emirp.h"
 
+static bool has_no_odd_factor(N number)
+{
+  return ((number % 3) != 0) &&
+          ((number % 5) != 0);
+}
+
+
+
 bool is_emirp(N number) {
   static const N base = 10U;
   return (number >= base) &&
           ((number % 2) != 0) &&
-          ((number % 3) != 0) &&
-          ((number % 5) != 0) &&
+          has_no_odd_factor(number) &&
           ((number % base) != (number / base));
 }
