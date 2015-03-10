b99db722e88d45362ff615035fbd9f1d43d173f5
David Schneider <d.schneider@dimoco.at>
2015-03-10 14:50:54 +0100

    reversed_digit_has_no_factors_and_is_not_palindromic

--- emirp.c
+++ emirp.c
@@ -17,33 +17,30 @@ static bool has_no_factors(N number)
 
 #define BASE 10U
 
-static bool is_not_palindromic(N number)
+static bool reversed_digit_has_no_factors_and_is_not_palindromic(N number)
 {
   static const N base2 = BASE * BASE;
-  N max_base = (number < base2) ? BASE : base2;
-  return ((number % BASE) != (number / max_base));
-}
-
-static bool reversed_digit_has_no_factors(N number)
-{
-  static const N base2 = BASE * BASE;
+  N lowest_digit = number % BASE;
+  N hightes_digit;
   N reversed;
   if (number < base2)
   {
-    reversed = (number % 10) * BASE + (number / 10);
+    hightes_digit = number / BASE;
+    reversed = lowest_digit * BASE + hightes_digit;
   }
   else
   {
-    reversed = ((number % 10) * base2) +
+    hightes_digit = number / base2;
+    reversed = (lowest_digit * base2) +
             (((number / 10) % 10) * BASE) +
-            (number / base2);
+            hightes_digit;
   }
-  return has_no_factors(reversed);
+
+  return (lowest_digit != hightes_digit) && has_no_factors(reversed);
 }
 
 bool is_emirp(N number)
 {
   return has_no_factors(number) &&
-          reversed_digit_has_no_factors(number) &&
-          is_not_palindromic(number);
+          reversed_digit_has_no_factors_and_is_not_palindromic(number);
 }
